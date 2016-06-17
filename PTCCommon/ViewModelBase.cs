41e7b48cb4c2cdbd1c6aa4680d8b3d70b060f443
nkprasanna <nkprasasnna@gmail.com>
2016-06-17 10:47:37 +0530

    Reusability Using a View Model Base Class- Final One

---
+++ PTCCommon/ViewModelBase.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+
+namespace PTCCommon
+{
+    public class ViewModelBase
+    {
+        public ViewModelBase()
+        {
+            Init();
+        }
+        public bool IsDetailAreaVisible { get; set; }
+        public bool IsListArearVisible { get; set; }
+        public bool IsSearchArearVisible { get; set; }
+        public bool IsValid { get; set; }
+        public string Mode { get; set; }
+        public List<KeyValuePair<string, string>> ValidationErrors { get; set; }
+        public string EventArgument { get; set; }
+        public string EventCommand { get; set; }
+
+        protected virtual void ListMode()
+        {
+            IsValid = true;
+            IsDetailAreaVisible = false;
+            IsListArearVisible = true;
+            IsSearchArearVisible = true;
+            Mode = "List";
+        }
+
+        protected virtual void Init()
+        {
+            EventCommand = "list";
+            ListMode();
+            ValidationErrors = new List<KeyValuePair<string, string>>();
+            EventArgument = string.Empty;
+        }
+        protected virtual void AddMode()
+        {
+            IsDetailAreaVisible = true;
+            IsListArearVisible = false;
+            IsSearchArearVisible = false;
+            Mode = "Add";
+        }
+        protected virtual void EditMode()
+        {
+            IsDetailAreaVisible = true;
+            IsListArearVisible = false;
+            IsSearchArearVisible = false;
+            Mode = "Edit";
+        }
+        protected virtual void Get()
+        {
+
+        }
+        protected virtual void ResetSearch()
+        {
+
+        }
+        protected virtual void Add()
+        {
+            AddMode();
+        }
+        protected virtual void Edit()
+        {
+            EditMode();
+        }
+        protected virtual void Delete()
+        {
+            ListMode();
+        }
+        protected virtual void Save()
+        {
+
+            if (ValidationErrors.Count > 0)
+            {
+                IsValid = false;
+
+            }
+            if (!IsValid)
+            {
+                if (Mode == "Add")
+                {
+                    AddMode();
+                }
+                else
+                {
+                    EditMode();
+                }
+            }
+        }
+        public virtual void HandleRequest()
+        {
+            switch (EventCommand.ToLower())
+            {
+                case "list":
+                case "search":
+                    Get();
+                    break;
+                case "resetsearch":
+                    ResetSearch();
+                    Get();
+                    break;
+                case "save":
+                    Save();
+                    if (IsValid)
+                    {
+                        Get();
+                    }
+                    break;
+                case "edit":
+                    IsValid = true;
+                    Edit();
+                    break;
+                case "delete":
+                    ResetSearch();
+                    Delete();
+                    break;
+                case "add":
+                    Add();
+                    break;
+                case "cancel":
+                    ListMode();
+                    Get();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
--- PTCData/TrainingProductViewModel.cs
+++ PTCData/TrainingProductViewModel.cs
@@ -1,81 +1,41 @@
-﻿using System;
+﻿using PTCCommon;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PTCData
 {
-    public class TrainingProductViewModel
+    public class TrainingProductViewModel : ViewModelBase
     {
-        public TrainingProductViewModel()
+        public TrainingProductViewModel() : base()
+        {
+
+        }
+        protected override void Init()
         {
             Products = new List<TrainingProducts>();
             SearchEntity = new TrainingProducts();
             Entity = new TrainingProducts();
-            Init();
-        }
-
-        private void Init()
-        {
-            EventCommand = "list";
-            ListMode();
-            ValidationErrors = new List<KeyValuePair<string, string>>();
-            EventArgument = string.Empty;
+            base.Init();
         }
 
         public TrainingProducts Entity { get; set; }
-        public string EventCommand { get; set; }
+
         public List<TrainingProducts> Products { get; set; }
         public TrainingProducts SearchEntity { get; set; }
-        public bool IsDetailAreaVisible { get; set; }
-        public bool IsListArearVisible { get; set; }
-        public bool IsSearchArearVisible { get; set; }
-        public bool IsValid { get; set; }
-        public string Mode { get; set; }
-        public List<KeyValuePair<string, string>> ValidationErrors { get; set; }
-        public string EventArgument { get; set; }
 
-        public void HandleRequest()
+
+        public override void HandleRequest()
         {
             switch (EventCommand.ToLower())
             {
-                case "list":
-                case "search":
-                    Get();
-                    break;
-                case "resetsearch":
-                    ResetSearch();
-                    Get();
-                    break;
-                case "save":
-                    Save();
-                    if (IsValid)
-                    {
-                        Get();
-                    }
-                    break;
-                case "edit":
-                    IsValid = true;
-                    Edit();
-                    break;
-                case "delete":
-                    ResetSearch();
-                    Delete();
-                    break;
-                case "add":
-                    Add();
-                    break;
-                case "cancel":
-                    ListMode();
-                    Get();
+                case "prasanna":
                     break;
                 default:
                     break;
             }
+            base.HandleRequest();
         }
-
-        private void Save()
+        protected override void Save()
         {
             TrainingProductManager mgr = new TrainingProductManager();
 
@@ -90,82 +50,43 @@ namespace PTCData
 
             ValidationErrors = mgr.ValidationErrors;
 
-            if (ValidationErrors.Count > 0)
-            {
-                IsValid = false;
-
-            }
-            if (!IsValid)
-            {
-                if (Mode == "Add")
-                {
-                    AddMode();
-                }
-                else
-                {
-                    EditMode();
-                }
-            }
+            base.Save();
         }
-
-        private void AddMode()
-        {
-            IsDetailAreaVisible = true;
-            IsListArearVisible = false;
-            IsSearchArearVisible = false;
-            Mode = "Add";
-        }
-        private void EditMode()
-        {
-            IsDetailAreaVisible = true;
-            IsListArearVisible = false;
-            IsSearchArearVisible = false;
-            Mode = "Edit";
-        }
-        private void Add()
+        protected override void Add()
         {
             IsValid = true;
             Entity = new TrainingProducts();
             Entity.IntroductionDate = DateTime.Now;
             Entity.Url = "http://";
             Entity.Price = 0;
-            AddMode();
+            base.Add();
         }
-        private void Edit()
+        protected override void Edit()
         {
             TrainingProductManager mgr = new TrainingProductManager();
             Entity = mgr.Get(Convert.ToInt32(EventArgument));
-            EditMode();
+            base.Edit();
         }
-
-        private void ListMode()
-        {
-            IsValid = true;
-            IsDetailAreaVisible = false;
-            IsListArearVisible = true;
-            IsSearchArearVisible = true;
-            Mode = "List";
-        }
-        private void Delete()
+        protected override void Delete()
         {
             TrainingProductManager mgr = new TrainingProductManager();
             Entity = new TrainingProducts();
             Entity.ProductID = Convert.ToInt32(EventArgument);
             mgr.Delete(Entity);
             Get();
-            ListMode();
+            base.Delete();
         }
-
-
-        private void ResetSearch()
+        protected override void ResetSearch()
         {
             SearchEntity = new TrainingProducts();
+            base.ResetSearch();
         }
 
-        private void Get()
+        protected override void Get()
         {
             TrainingProductManager mgr = new TrainingProductManager();
             Products = mgr.Get(SearchEntity);
+            base.Get();
         }
     }
 }
